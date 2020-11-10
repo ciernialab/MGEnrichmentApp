1825c34f8e2b6d5283615496404dbbb085aa8b2f
Justin Jao <jao.c.justin@gmail.com>
2020-11-10 12:56:41 -0800

    added rest of files to repo

---
+++ Help_HTML.R
@@ -0,0 +1,78 @@
+displayHelp <- HTML('
+<p style="font-size:30px">
+<b>Welcome to the Ciernia Lab Microglia Enrichment Calculator!</b>
+</p>
+<p>
+The calculator is quite simple; the user uploads the gene sets they want to test for enrichment. The app then runs what is essentially a one-tailed Fishers Exact Test with the user\'s uploaded genes for every gene set (currently 171 sets) in the database (against a specified "background universe" of genes). If the user\'s uploaded genes have a significant overlap with any particular gene set, the gene set is said to be highly represented, or "enriched." Depending on the particular gene set that is enriched, this could have important implications for the user\'s gene set.
+</p>
+<img src="venn_diagram.png" style="width:800px;height:400;" class="center">
+
+<h3 style = "font-size: 20px;font-weight: bold"> Tips For Using the App </h3>
+<br>
+<h5 style = "font-weight: bold">Uploading Gene Sets</h5>
+<ul>
+<li>The app can take in a gene set via text input in the textbox, or by uploading a dataset (acceptable file formats are csv, tsv and txt)</li>
+<li>The app can take in Ensembl, Entrez and MGI symbol gene IDs, but can only parse one type at a time, so all gene IDs need to follow the same ID type, and you must correctly specify which ID type you are using for the matching to work properly.</li>
+<li>If possible, it is recommended to use Ensembl IDs, as this is what was originally used to compile our gene set lists</li>
+<li> If you are pasting in IDs, they can be separated by tabs, spaces or commas.</li>
+</ul>
+
+<h5 style = "font-weight: bold">Selecting Background Gene Set</h5>
+<ul>
+<li>You can either set the background number of genes to be all mouse genes <i>(All mm10 Genes)</i>, or all the genes currently in the database <i>(All Genes in the Database)</i>.</li>
+<li> You can also upload a custom set by selecting <i>Custom</i>. Format for the custom background set follows the user-uploaded gene set format.</li>
+</ul>
+
+<h5 style = "font-weight: bold">Uploading Background Gene Set</h5>
+<ul>
+<li>Any custom uploaded background gene sets must be large enough to serve as a background gene set (it should be larger than the largest gene set in the database (7266 genes) + your user uploaded gene set). See online documentation for more information. </li>
+
+</ul>
+<h5 style = "font-weight: bold">Filtering & Disabling IDs</h5>
+
+<ul>
+<li>The p-value slider can be used to filter your results for FDR significance level. </li>
+<li>The checkbox options can be used to remove the specified columns from the output, which may be useful if your gene set is very long.
+</ul>
+
+<h5 style = "font-weight: bold">Querying Genes</h5>
+
+<ul>
+<li>Once you’re done toggling the various settings, you can click <b>Query Genes</b>, and the app will generate the resulting enrichment table,</li>
+<li> Results are only generated when the <b>Query Genes</b> button is clicked. If you make any further setting changes after generating results, recalculations will only come into effect when you click the button again to regenerate results.</li>
+</ul>
+
+<h5 style = "font-weight: bold">Downloading Results</h5>
+<ul>
+
+<li>Click the "Download Results" button to export your results to a csv file.</li>
+
+<li>Note that the results download as is (whatever filtering settings you\'ve used carry over - what you see is what you get).</li>
+</ul>
+
+<h3 style = "font-size: 20px;font-weight: bold">Interpreting Results</h3>
+<ul>
+<li><b>listname</b> - name of the gene set that is being compared against.</li>
+<li><b>pvalue</b> - p-value of enrichment analysis (as calculated by one-tailed Fisher\'s Exact Test).</li>
+<li><b>OR</b> - the Odds Ratio.</li>
+<li><b>notAnotB</b> - number of genes not in any gene set.</li>
+
+<li><b>inAnotB</b> - number of genes in the user\'s uploaded gene set (set A) but not in the current database gene set being analyzed (set B).</li>
+<li><b>inBnotA</b> - number of genes in the current database gene set (set B) but not in the user uploaded gene set (set A).</li>
+<li><b>inBinA</b> - number of genes in both the current database gene set and the user uploaded gene set.</li>
+<li><b>intersection_IDs</b> - the direct list of overlapping gene IDs (inBinA). This will be in the gene ID type that you have selected, and is the original overlap list, so use this for final results.</li>
+<li><b>intersection_ensembl</b> - the intersection_IDs, converted to ensembl, if possible.</li>
+<li><b>intersection_mgi_symbol</b> - the intersection_IDs, converted to MGI symbols, if possible.</li>
+<li><b>intersection_entrez</b> - the intersection_IDs, converted to entrez, if possible.</li>
+<li><b>FDR</b> - false discovery rate correction value.</li>
+<li><b>description</b> - brief description of gene set.</li>
+<li><b>source</b> - literature source of gene sets, if applicable.</li>
+<li><b>groups</b> - gene set category grouping.</li>
+</ul>
+The <b>intersection_ids</b> is the original list of overlapping gene IDs, in the format you uploaded and selected for your genes. The 3 successive columns after try to map the gene ID to its corresponding alternate ID equivalents, if possible (therefore one of the columns will be redundant, as it will be in the same gene ID type as what you uploaded, and due to [mapping](#differing-results-based-on-gene-id) problems, it is not fully complete). It is provided only for convenience to potentially lookup genes of interest faster. When reporting results, use the intersection_ids column for the most accurate results, and convert to other ID types through other means, if necessary.
+<br>
+<br>
+  For more details, please visit the online documentation on the GitHub <a href="https://github.com/ciernialab/MGEnrichmentApp">repository</a>.
+  <br>
+  <br>
+')
---
+++ app.R
@@ -0,0 +1,407 @@
+# App Description & Comments ----------------------------------------------
+# Microglia Enrichment Calculator
+# this Shiny app takes in a set of Gene IDs, and runs a enrichment analysis
+# (i.e. fisher's exact test) based on the database of microglia-relevant genes
+# The database loads data previously generated from another script (NewGeneLists.R)
+# so that the only thing described here is the app itself
+
+# If needed, the RStudio Shiny tutorial may be useful in understanding
+# how this app was designed:
+# https://shiny.rstudio.com/tutorial/
+
+# Load Datasets & Packages ------------------------------------------------
+
+library(shiny)
+library(tidyverse)
+library(GeneOverlap)
+library(vroom)
+library(DT)
+library(shinydashboard)
+library(BiocManager)
+options(repos = BiocManager::repositories())
+getOption("repos")
+
+#If needed, the datasets can be regenerated using the NewGeneLists.R script
+
+# there are 5 key datasets used in this script
+# mouse_genes - contains all the mouse genes
+# masterlist - contains all the MG-relevant genes, and their corresponding gene IDs
+# ensemblList, entrezList, mgiList - (the MG-relevant genes sorted into gene lists) for each respective gene ID
+
+#load in datasets from here:
+load(file="GeneLists.RData")
+
+#load(file = "~/Google Drive/4th Year/Ciernia Lab/Shiny_Applications/Annie's_App/geneListDatabaseFiles.RData")
+#mouse_genes <- read.csv("~/Google Drive/4th Year/Ciernia Lab/Shiny_Applications/MGEnrichmentApp/All_ensemble_mm10genes.csv", stringsAsFactors = F)
+
+
+#initializes default mm10 values
+mm10genome <- length(unique(mouse_genes$mgi_symbol))
+mm10genes <- length(unique(masterlist$ensembl_gene_id))
+
+# Designing User Interface ---------------------------------------------------------------
+# this section describes all the graphical user interface of the app
+
+# load HTML for Help page
+source("Help_HTML.R")
+
+ui <- dashboardPage(
+    
+    
+    # Application title
+    dashboardHeader(title = "Microglia Gene Set Enrichment Calculator", titleWidth = 430),
+    
+    # Sidebar Layout
+    # this contains all the UI for the sidebar
+    # the different elements are self-explanatory
+    dashboardSidebar(width = 430,
+                     tags$style(".skin-blue .sidebar a { color: #444; }"),
+                     textAreaInput("txtGeneID", label = "Input your genes of 
+                          interest here (must all be the same gene ID format)",
+                                   placeholder = "CxCl2, 344521, ENSMUSG00000000202,..."),
+                     fileInput("fileGeneID", "or upload your gene list here", accept = c(".csv", ".tsv", ".txt")),
+                     radioButtons("typeID", "Which gene ID are you using?",
+                                  choices = c("Ensembl" = "ensembl_gene_id",
+                                              "Entrez" = "entrezgene_id",
+                                              "MGI Symbol" = "mgi_symbol")),
+                     radioButtons("background", "Set the background query:",
+                                  choices = c("All mm10 Genes" = "reference",
+                                              "All Genes in the Database" = "intersection",
+                                              "Custom" = "custom")),
+                     
+                     # conditionalPanel uses a javascript expression to only display the background gene
+                     # upload inputs when "custom" is selected in the previous radioButton selection
+                     conditionalPanel(condition = "input.background == 'custom'",
+                                      textAreaInput("txtBackgroundID", label = "Input your background genes of 
+                          interest here",
+                                                    placeholder = "CxCl2, 344521, ENSMUSG00000000202,..."),
+                                      fileInput("fileBackgroundID", "or upload your background list here", accept = c(".csv", ".tsv", ".txt"))),
+                     checkboxGroupInput("displayID", "Disable Intersection Gene IDs?", inline = TRUE,
+                                        choiceNames = c("Intersection IDs","Ensembl", "MGI Symbol", "Entrez"),
+                                        choiceValues = c("intersection_IDs", "intersection_ensembl", "intersection_mgi_symbol", "intersection_entrez"), 
+                                        selected = NULL),
+                     sliderInput("pval", "Change Minimum FDR-value (1.0 means no filtering):", min = 0.01, max = 1.00, value = 1.00, step = 0.01),
+                     div(style="display: inline-block;vertical-align:left; height:120px; width: 120px;",actionButton("searchGene", label = "Query Genes")),
+                     div(style="display: inline-block;vertical-align:left; height: 120px; width: 120px;",downloadButton("dwnld", "Download Results"))
+    ),
+    
+    
+    # displays everything in the center of the page (i.e. the table of results)
+    dashboardBody(
+        
+        
+        tabsetPanel(type = "tabs",
+                    tabPanel("Table",  DT::dataTableOutput("enrichmentTable")),
+                    tabPanel("Help", displayHelp)),
+        
+        div(style = 'overflow-x: scroll', DT::dataTableOutput('tbl')),
+        
+        tags$head(tags$style(HTML('
+       
+       .skin-blue .main-header .logo {
+                              background-color: #407f87
+                              }
+                              .skin-blue .main-header .logo:hover {
+                              background-color: #407f87;
+                              }
+
+        /* navbar (rest of the header) */
+        .skin-blue .main-header .navbar {
+                              background-color:#4c969f;
+                              }        
+
+        /* main sidebar */
+        .skin-blue .main-sidebar {
+                              background-color: #253538;
+                              }
+
+        /* toggle button when hovered  */                    
+         .skin-blue .main-header .navbar .sidebar-toggle:hover{
+                              background-color:#4c969f;
+                              }
+                              ')))
+        
+        # outputs the entered gene IDs into the console when the app is run
+        # can be enabled when needed as useful for troubleshooting any issues with IDs
+        # textOutput("geneText")
+        
+    ) 
+)
+
+# Server Functions --------------------------------------------------------
+# this section describes all the data underlying how the database is built
+
+# the tutorial on reactivity may be useful in understanding this section:
+# https://shiny.rstudio.com/tutorial/written-tutorial/lesson6/
+
+server <- function(input, output, session) {
+    
+    # switch function is used to change which list is used, depending on user input
+    # https://www.datamentor.io/r-programming/switch-function/
+    # input: typeID (user input)
+    # output: character (either "ensembl_gene_id", "mgi_symbol", etc)
+    # the character output is then used in later functions to determine which ID to use
+    switchList <- reactive({
+        switch(input$typeID, 
+               ensembl_gene_id = "ensembl_gene_id",
+               entrezgene_id = "entrezgene_id",
+               mgi_symbol = "mgi_symbol")
+    })
+    # Overlap Function --------------------------------------------------------
+    
+    # this is the overlap function used to run the analysis
+    # targetlist name is the gene list uploaded by user
+    # genelists = Lists is the database of MG genes we currently are using
+    # genomesize is the size of the list of genes used (which varies based on gene ID type)
+    
+    Overlap_fxn <- function(targetlistname,genelists = List,genomesize){
+        
+        #eventual output
+        out <- NULL
+        
+        #rename for ease of use
+        target <- targetlistname  
+        
+        # loops through each gene list in List and generates overlap results
+        for (i in 1:length(genelists)) { 
+            
+            #call gene overlaps
+            go.obj <- newGeneOverlap(target,
+                                     genelists[[i]],
+                                     genome.size=genomesize)
+            
+            #perform stattistical tests
+            go.obj <- testGeneOverlap(go.obj) # returns onetailed pvalue
+            #return odds ratio:
+            OR <- getOddsRatio(go.obj)
+            pvalue <- getPval(go.obj)
+            
+            #extract contingency table
+            CT <- getContbl(go.obj)
+            notAnotB <- CT[1,1]
+            inAnotB <- CT[1,2]
+            inBnotA <- CT[2,1]
+            inBinA <- CT[2,2]
+            
+            #merges results to a table
+            CTlist <- cbind(notAnotB,inAnotB,inBnotA,inBinA)
+            
+            #get list of intersections - this will get different ids based on user input.
+            intersection <- go.obj@intersection
+            intersection_IDs <- paste(as.character(intersection),collapse=", ",sep="")
+            
+            # switchList is used to determine which column to pull from the original
+            # mouse_genes dataframe for the next few lines of code
+            # e.g. if user uploaded entrez IDs, it will pull the overlapping 
+            # entrez ID rows in the mouse_genes entrez ID column,
+            # then it will look to see if there is a corresponding match of other IDs in the same row
+            
+            # get Ensembl IDs
+            intersection_ensembl <- mouse_genes$ensembl_gene_id[which(pull(mouse_genes, switchList()) %in% intersection)]
+            intersection_ensembl <- paste(as.character(intersection_ensembl),collapse=", ",sep="")
+            
+            #get intersection gene names
+            intersection_mgi_symbol <- mouse_genes$mgi_symbol[which(pull(mouse_genes, switchList()) %in% intersection)]
+            intersection_mgi_symbol <- paste(as.character(intersection_mgi_symbol),collapse=", ",sep="")
+            
+            # get entrez IDs
+            intersection_entrez <- mouse_genes$entrezgene_id[which(pull(mouse_genes, switchList()) %in% intersection)]
+            intersection_entrez <- paste(as.character(intersection_entrez), collapse = ", ", sep ="")
+            
+            #get listname
+            listname <- paste(names(genelists[i]))
+            
+            #combine results
+            results <- cbind(listname,pvalue,OR, CTlist,intersection_IDs, intersection_ensembl,intersection_mgi_symbol, intersection_entrez)
+            
+            #colnames(results) <- c("listname","pvalue","OR","notAnotB","inAnotB","inBnotA","inBinA","intersection_IDs", "ensembl","MGI", "entrez")
+            
+            # bind to output
+            out <- rbind(out,results) 
+        }
+        
+        #cast to dataframe
+        out <- as.data.frame(out)
+        
+        #cast to numeric (necessary for the sorting feature in the application)
+        out$OR <- as.numeric(out$OR)
+        out$notAnotB <- as.numeric(out$notAnotB)
+        out$inAnotB <- as.numeric(out$inAnotB)
+        out$inBnotA <- as.numeric(out$inBnotA)
+        out$inBinA <- as.numeric(out$inBinA)
+        
+        rownames(out) <- NULL
+        
+        #return results
+        return(out)
+    }
+    
+    
+    # Running Analysis --------------------------------------------------------
+    # The giant reactive function called to calculate results.
+    # Could benefit from splitting into smaller helper functions...
+    result_calculation <- reactive({
+        
+        # Reading in User Input & Matching Lists ---------------------
+        # here we define the functions used to interpret user input, and to determine
+        # which corresponding type of list (ensembl, entrez, etc) to use
+        
+        #function which reads the file the user uploads
+        geneQueryFile <- reactive({
+            
+            #when a user uploads a file to the app, the file is given a temporary directory
+            #which can be accessed by input$fileGeneID$datapath
+            #input$fileGeneID$name can be used to access the file type
+            
+            #get file type
+            ext <- tools::file_ext(input$fileGeneID$name)
+            
+            #switch is used to determine which read function to use based on filetype
+            upldFile <- switch(ext,
+                               csv = read.csv(input$fileGeneID$datapath, sep = ",", stringsAsFactors = FALSE),
+                               tsv = read_tsv(input$fileGeneID$datapath),
+                               txt = read.delim(input$fileGeneID$datapath, sep = "\t", stringsAsFactors = FALSE))
+            
+            #ensure only one column is read in
+            upldFile <- upldFile[,1]
+            return(upldFile)
+        })
+        
+        #reads in textbox details. When text is entered, it is registered as one giant string
+        #this uses a regex to parse the string (either by comma, space or a new line)
+        #and then unlists it (since strsplit outputs a string type)
+        geneQueryTxt <- reactive(unlist(strsplit(input$txtGeneID, split = "[, \n]+")))
+        
+        #if statement to decide whether to use file or textbox input.
+        #  Uses textbox input only when file input is null.
+        geneQueryDecide <- if (is.null(input$fileGeneID)) {
+            geneQueryTxt()
+        } else {
+            geneQueryFile()
+        }
+        
+        #switch is used to determine which gene List in our database to use,
+        # based on the gene ID type specified by the user
+        matchList <- if (switchList() == "ensembl_gene_id") {
+            List <- ensemblList
+        } else if (switchList() == "mgi_symbol") {
+            List <- mgiList
+        } else {
+            List <- entrezList
+        }
+        
+        #switch is used to determine which mm10genome size to use based on which
+        # gene ID type is specified by the user. The size varies by gene ID type
+        matchMm10Genome <- if (switchList() == "ensembl_gene_id"){
+            mm10genome <- length(unique(mouse_genes$ensembl_gene_id))
+        } else if (switchList() == "entrezgene_id") {
+            mm10genome <- length(unique(mouse_genes$entrezgene_id))
+        } else if (switchList() == "mgi_symbol") {
+            mm10genome <- length(unique(mouse_genes$mgi_symbol))
+        }
+        
+        #switch is used to determine which mm10gene to use
+        matchMm10Genes <- if (switchList() == "ensembl_gene_id"){
+            mm10genes <- length(unique(unlist(ensemblList)))
+        } else if (switchList() == "entrezgene_id") {
+            mm10genes <- length(unique(unlist(entrezList)))
+        } else if (switchList() == "mgi_symbol") {
+            mm10genes <- length(unique(unlist(mgiList)))
+        }
+        
+        # Reading Custom Uploaded Background Lists -----------
+        
+        #similar to the user uploaded gene set functions above.
+        backgroundQueryFile <- reactive({
+            
+            ext <- tools::file_ext(input$fileBackgroundID$name)
+            upldFile <- switch(ext,
+                               csv = read.csv(input$fileBackgroundID$datapath, sep = ",", stringsAsFactors = FALSE),
+                               tsv = read_tsv(input$fileBackgroundID$datapath),
+                               txt = read.delim(input$fileBackgroundID$datapath, sep = "\t", stringsAsFactors = FALSE))
+            
+            upldFile <- upldFile[,1]
+            return(upldFile)
+        })
+        
+        backgroundQueryTxt <- reactive(unlist(strsplit(input$txtBackgroundID, split = "[, \n]+")))
+        
+        backgroundQueryDecide <- if (is.null(input$fileBackgroundID)) {
+            backgroundQueryTxt()
+        } else {
+            backgroundQueryFile()
+        }
+        #browser()
+        #counts number of entries in uploaded background set, 
+        #to be used in genomesize of overlap function
+        backgroundQuerySize <- length(unique(backgroundQueryDecide))
+        
+        # Calculating Results -----------------------------------------------------
+        # the code for actually running the functions
+        
+        #run overlap function
+        results <- Overlap_fxn(targetlistname = unique(geneQueryDecide), genelists = List,
+                               genomesize = switch(input$background,
+                                                   "reference" = mm10genome,
+                                                   "intersection" = mm10genes,
+                                                   "custom" = backgroundQuerySize))
+        
+        #adjust pvalue
+        results$pvalue <- as.numeric(formatC(x = as.numeric(as.character(results$pvalue)), format = "E"))
+        #calculate FDR adjusted p-value (this column is later dropped)
+        results$FDRdrop <- p.adjust(results$pvalue, method='fdr')
+        
+        #formats FDR value to scientific notation (for some reason, the reformatting only displayed
+        # if I reformatted on a second column, which is why I drop the previous column)
+        results$FDR <- as.numeric(formatC(x = as.numeric(results$FDRdrop), format = "E"))
+        
+        #gets gene set list information
+        p <- masterlist %>%
+            dplyr::select(-ensembl_gene_id, -graphname, -ID) %>%
+            distinct()#.keep_all = T)
+        
+        #change one of the gene list names to be correct
+        #p$description[91] <- "differential gene expression polyI:C MIA on GD14, whole brain microglia P0"
+        
+        #merge the result dataframe and list information dataframe together
+        merged_results <- merge(results, p, by=c("listname"), all.x=T)
+        merged_results <- unique(merged_results)
+        
+        #this filters the dataframe to remove any values <= user input p value
+        merged_results <- merged_results %>% 
+            filter(FDRdrop <= input$pval) %>% 
+            #drops the dummy FDR column
+            select(-FDRdrop)
+        
+        return(merged_results)
+    })
+    
+    # Displaying Output -------------------------------------------------------
+    
+    # removes the intersection IDs specified by user
+    remove_ids <- reactive(select(result_calculation(), -input$displayID))
+    
+    # eventReactive delays any querying until you click "Query Gene", since
+    # if we have a lot of genes this could be a hassle if every change of a setting
+    # caused a recalculation of results
+    run_calc <- eventReactive(input$searchGene, remove_ids())
+    
+    #renders the table
+    output$enrichmentTable <- DT::renderDataTable(datatable(run_calc(), options = list("lengthMenu"=c(10, 25, 50, 100, 200), "scrollX" = TRUE))) #%>% 
+    #formatStyle("intersection", lineHeight='80%'))
+    
+    # Download Results --------------------------------------------------------
+    
+    #download results
+    output$dwnld <- downloadHandler(
+        filename = function() {
+            "MG_Enrichment_Results.csv"
+        },
+        content = function(file) {
+            write.csv(remove_ids(), file)
+        }
+    )
+}
+
+#function to run the whole app
+shinyApp(ui, server)

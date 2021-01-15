f65fa6742341c4f7ca8f118f448a38e36dedd56d
Annie Vogel Ciernia <a.ciernia@gmail.com>
2021-01-15 06:57:34 -0800

    updated with new example
    do we need the RData object? Can we just load the all mouse genes separately???

--- NewGeneLists.R
+++ NewGeneLists.R
@@ -1,47 +1,68 @@
 # Script Comments ---------------------------------------------------------
-# this is a script that can be used to regenerate some of datasets used in the
-# MGEnrichment app as required whenever they need to be updated
+# this is a script that can be used to update/add new lists to the microglia gele list database
+# The new list is then used as input into the MGEnrichment app
 # simply update the spreadsheet that is read in as "masterlist"
 
 library(biomaRt)
 library(here)
 library(tidyverse)
 
+##################################################################################
+# read in the current mg genelists - this is a manually curated dataset containing MG relevant genes
+# every time the spreadsheet is updated, it can be reloaded here
+##################################################################################
+masterlist <- read.csv(here("Microglia.Mouse.GeneListDatabase.11.21.2020.csv"))[,-1]
+
+
+##################################################################################
+#Read in your new gene lists to add and format them with the same columns as the masterlist
+#then append your new gene lists to the master list and save.
+#the master list has the following columns:
+#[1] "ensembl_gene_id" "mgi_symbol"      "hgnc_symbol"     "entrezgene_id"   "listname"        "description"    
+#[7] "source"          "groups"          "Species"         "tissue"          "shortname" 
+
+#if your gene list does not have all of these columsn then use biomart to help create them.
+##################################################################################
+#Example: 
+
+#read in gene list from supplemental table of published paper. In this example this list is genes with increased expression
+#in microglia from the cerebellum compared to the striatum
+CBgreaterSTR <- read.csv("Moregenes.csv")
+
+
+#select column with the ensemble gene id
+CBgreaterSTR <- CBgreaterSTR %>% select(gene.id)
+
+#use biomart to add in other gene information:
 # get mouse ensemble data
 mouse <- useMart("ensembl", dataset = "mmusculus_gene_ensembl")
 
 # query ensemble and entrez ids - this is the dataset with all mouse genes
 mouse_genes <- getBM(attributes = c("ensembl_gene_id","mgi_symbol", "hgnc_symbol", "entrezgene_id"),
-               mart = mouse)
+                     mart = mouse)
+
+#combine biormart informtion wtih gene list
+ensgenes <- merge(CBgreaterSTR,mouse_genes,by.x="gene.id",by.y="ensembl_gene_id")
+colnames(ensgenes)[1] <- c("ensembl_gene_id")
 
 
-# read in mg genelists - this is a manually curated dataset containing MG relevant genes
-# every time the spreadsheet is updated, it can be reloaded here
-masterlist <- read.csv(here("Microglia.Mouse.GeneListDatabase.11.21.2020.csv"))[,-1]
+#add in metadata
+ensgenes$listname <- c("Cerebellum > Striatum MG")  
+ensgenes$description <- c("genes with higher expression in cerebellum microglia compared to stratium microglia, TRAP isolation") 
+ensgenes$source <- c("Ataya, et al., 2018")   
+ensgenes$groups <- c("Microglia") 
+ensgenes$Species <- c("mouse") 
+ensgenes$tissue <- c("microglia") 
+ensgenes$shortname <- c("Cerebellum > Striatum MG") 
 
 
-##################################################################################
-#add in new genelists - Example
-#replace datasets with your own csv files
-##################################################################################
-#DAM MG
 
-#must have the following columns:
-#[1] "ensembl_gene_id" "mgi_symbol"      "hgnc_symbol"     "entrezgene_id"   "listname"        "description"    
-#[7] "source"          "groups"          "Species"         "tissue"          "shortname" 
-
-### sample datasets
-DAM1 <- read.csv("DAMgreaterHOM.MG.csv")
-DAM2 <- read.csv("DAMlessHOM.MG.csv")
-DAM3 <- read.csv("DAMstage1greaterDAMstage2.MG.csv")
-DAM4 <- read.csv("DAMstage1lessDAMstage2.MG.csv")
-
-DAM <- rbind(DAM1,DAM2,DAM3,DAM4)
-
-DataBasemm10 <- rbind(masterlist,DAM)
+#append new gene list to the database and write out 
+DataBasemm10 <- rbind(masterlist,ensgenes)
 
 write.csv(DataBasemm10,file="Microglia.Mouse.GeneListDatabase.newdatehere.csv")
 
+#--------------------------------
 #make summary file of all gene counts per list
 summ.files <- DataBasemm10[,c(1,5:ncol(DataBasemm10))]
 
@@ -54,14 +75,11 @@ write.csv(summ.files, file= "Genelists.in.mm10Database.newdatehere.csv")
 masterlist <- DataBasemm10
 
 #--------------------------------
-  
-#write out new R objects for comparisons for each gene ID type:
-# mergedGenes <- merge(mouse_genes, DataBasemm10, by = "ensembl_gene_id", all.x = T, all.y = T)
-
+#write out new R objects for the App. Include
 
 # save data output (only masterlist and mouse_genes, the other 3 are generated in app)
-save(mouse_genes, masterlist,
-     file = here("GeneLists.RData"))
+#save(mouse_genes, masterlist,
+ #    file = here("GeneLists.RData"))
 
 
 
